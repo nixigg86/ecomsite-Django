341bf56987569bff99bc5e1c3ce0a7ef88ad410d
nixigg86 <dev.py86@gmail.com>
2021-03-02 20:21:10 -0800

    adding checkout page

--- shop/views.py
+++ shop/views.py
@@ -22,6 +22,9 @@ def index(request):
 
 def detail(request, id):
     product_object = Products.objects.get(id=id)
-    return render(request, 'shop/detail.html', {"product_object": product_object})    
+    return render(request, 'shop/detail.html', {"product_object": product_object})   
+
+def checkout(request):
+    return render(request, 'shop/checkout.html')     
 
 
